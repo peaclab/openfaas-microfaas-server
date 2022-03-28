6926d0db653622c1114d2634c696e9a2285293b4
Allen Zou <allenzou@bu.edu>
2022-03-28 16:31:01 -0400

    added reboot capabilities

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ type Payload struct {
 	Src    string `json:"src"`
 	Params string `json:"params,omitempty"`
 	Lang   string `json:"lang"`
+	Worker string `json:"worker"`
 }
 type Response struct {
 	Fid         string        `json:"fid"`
@@ -41,7 +42,7 @@ func handleRequests() {
 	myRouter.HandleFunc("/showfunctions", returnAllFunctions)
 	myRouter.HandleFunc("/showfunctions/{fid}", returnFunction)
 	myRouter.HandleFunc("/run", runFunction).Methods("POST")
-
+	updateHealth()
 	log.Fatal(http.ListenAndServe(":8080", myRouter))
 }
 func returnFunction(w http.ResponseWriter, r *http.Request) {
@@ -55,6 +56,7 @@ func returnFunction(w http.ResponseWriter, r *http.Request) {
 	}
 }
 func runFunction(w http.ResponseWriter, r *http.Request) {
+	// job_received = true
 	reqBody, _ := ioutil.ReadAll(r.Body)
 	var payload Payload
 	res := Response{}
@@ -83,6 +85,13 @@ func runFunction(w http.ResponseWriter, r *http.Request) {
 		} else {
 			res = Response{Fid: payload.Fid, TimeElapsed: t.Sub(start), Result: string(out)}
 		}
+					// // Testing out rebooting after a function
+	// lol := exec.Command("echo", "b", ">", "/proc/sysrq-trigger")
+	lol := exec.Command("reboot")
+
+	out_lol, _ := lol.CombinedOutput()
+	fmt.Println(out_lol)
+
 
 	} else if strings.ToLower(payload.Lang) == "python" {
 		start := time.Now()
@@ -102,17 +111,51 @@ func runFunction(w http.ResponseWriter, r *http.Request) {
 
 	json.NewEncoder(w).Encode(res)
 
+
+
 	//fmt.Fprintf(w, "%+v", string(reqBody))
 }
 func returnAllFunctions(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: returnAllFunctions")
 	json.NewEncoder(w).Encode(Payloads)
 }
+func updateHealth(){
+		client := http.Client{
+		Timeout: 5 * time.Second,
+	}
+	fmt.Println("Sending health response.")
+	resp, err := client.Get(PROVIDER_IP)
+	
+	if (err == nil){
+		fmt.Println("Received health response.")
+		fmt.Println(resp)
+		return
+	} else {
+		updateHealth()
+	}
+}
+//timer funciton
+// func job_timer(){
+// 	for {
+// 		t1 := time.NewTimer(TIMEOUT)
+// 	if( >= TIMEOUT)
+		//resend health check
+		//reset timer
+	//else if job received
+// 	}
+// }
+var PROVIDER_IP = "http://192.168.1.203:8083/healthz"
+// var job_received = false
+// var TIMEOUT = 10
+// var timer
 func main() {
 	fmt.Println("Rest API v2.0 - Mux Routers")
 	Payloads = []Payload{
 		{Fid: "helloworld", Src: "print(\"hello\")", Lang: "Python", Params: "{\"A\":\"3\", \"B\":\"5\"}"},
 		{Fid: "helloworld2", Src: "print(\"hello\")", Lang: "Python", Params: "{\"A\":\"4\", \"B\":\"6\"}"},
 	}
+
+	// go job_timer();
 	handleRequests()
+
 }
